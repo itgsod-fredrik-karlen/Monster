915ed2cb196c09c1cf202f63fd39b110a072e5ba
Fredrik Karlén <fredrik.karlen@elev.it-gymnasiet.se>
2012-12-06 16:21:20 +0100

    added the start of a hunger system

--- monster.rb
+++ monster.rb
@@ -1,33 +1,41 @@
 class Monster
 	
 	def initialize
-		@colors = ["red", "blue", "green", "purple"]
-		@sizes = ["tiny", "smal", "medium sized", "big", "huge", "gigantic", "gianormus"]
+		@colors = ["red", "blue", "green", "purple", "pink", "orange", "brown", "invisible", "yellow", "white", "black", "peachpuff colored"]
+		@sizes = ["microscopic", "tiny", "smal", "medium sized", "big", "huge", "gigantic", "gianormus", "realy, realy gianormus"]
 		@scarelevel = ["not at all", "a bit", "pretty", "very", "extreamly"]
 		@foods = ["lasanga", "meatloaf", "pancakes", "a car", "spagetti"]
+		@hunger_levels = ["dead", "starving","could use some food", "medium hungry", "stuffed and can't eat more", "about to explode"]
 		puts "what do you whant to name your monster?"
 		@name = gets.chomp.capitalize
 		@color = @colors[rand(@colors.size)]
 		@size = @sizes[rand(@sizes.size)]
 		@scary = @scarelevel[rand(@scarelevel.size)]
 		@food = @foods[rand(@foods.size)]
+		
 
 	end
 
 	def description
-		"#{@name} is a #{@scary} scary monster who is #{@color} and is a #{@size} monster"
+		"#{@name} is a #{@scary} scary monster who is #{@color} and is #{@size}"
 
+	end
+	def inc_hunger
+		@
 	end
 
 	def menu
-		loop do
-			sleep 2
-			puts "what would you like #{@name} to do now?
+		what = 0
+		until what == 5 do
+			sleep 0.5
+			puts "#{@name} is #{@hunger}
+			what would you like #{@name} to do now?
 			1. eat
 			2. sleep
 			3. scare people
-			4. scare other monsters"
-			
+			4. scare other monsters
+			5. say goodbye"
+
 			what = gets.to_i
 			if what == 1
 				puts "#{@name} is eating eating #{@food}"
@@ -37,8 +45,10 @@ class Monster
 				puts "scare people"
 			elsif what == 4
 				puts "scare monsters"
-			end
+			end	
 		end
+		sleep 0.5
+		puts "Goodbye! :D"
 	end
 
 	
