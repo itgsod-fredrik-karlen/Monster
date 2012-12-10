4edf512117294d00c8fead22815fe9bb1ee0abdd
Fredrik Karlén <fredrik.karlen@elev.it-gymnasiet.se>
2012-12-10 10:33:51 +0100

    added countries to scare people in

--- monster.rb
+++ monster.rb
@@ -6,7 +6,11 @@ class Monster
 		@scarelevel = ["not at all", "a bit", "pretty", "very", "extreamly"]
 		@foods = ["lasanga", "meatloaf", "pancakes", "a car", "spagetti"]
 		@hunger_levels = ["dead", "starving","could use some food", "medium hungry", "stuffed and can't eat more", "about to explode"]
-		puts "what do you whant to name your monster?"
+		@namerica = ["Americans with shotguns, succeded and survived.", "high Jamaicans, they didn't move a muscle.", "Canadians, succeded but got maplesirap all over it's body."]
+		@samerica = ["people from Argentina but they were busy dancing.", "people from Chile, they ran like hell.", "people from Bolivia, succeded and got to burn some drugs.",
+		"Brazilians, but they were busy playing football.", "the original inhabitants from Amazonas, #{@name} got a arrow in it's knee."]
+		@europe = ["Swedes, but they were ice cold...", "the French, but they didn't understand #{name}s language.", "" ]
+		puts "What do you whant to name your monster?"
 		@name = gets.chomp.capitalize
 		@color = @colors[rand(@colors.size)]
 		@size = @sizes[rand(@sizes.size)]
@@ -21,29 +25,45 @@ class Monster
 
 	end
 	def inc_hunger
-		@
+		
 	end
 
 	def menu
-		what = 0
-		until what == 5 do
-			sleep 0.5
-			puts "#{@name} is #{@hunger}
-			what would you like #{@name} to do now?
+		todo = 0
+		until todo == 5 do
+			sleep 6
+			#puts "#{@name} is #{@hunger}
+			puts "What would you like #{@name} to do now?
 			1. eat
 			2. sleep
 			3. scare people
 			4. scare other monsters
 			5. say goodbye"
 
-			what = gets.to_i
-			if what == 1
+			todo = gets.to_i
+			if todo == 1
 				puts "#{@name} is eating eating #{@food}"
-			elsif what == 2
+			elsif todo == 2
 				puts "sleeping"
-			elsif what == 3
-				puts "scare people"
-			elsif what == 4
+			elsif todo == 3
+				todo_scare = 0
+				puts "where do you whant to scare people?
+				1. North America
+				2. South America
+				3. Europe
+				4. Asia
+				5. Africa
+				6. Oceania
+				7. Antarctica"
+				todo_scare = gets.to_i
+				if todo_scare == 1
+					puts "#{@name} tried to scare #{@namerica[rand(@namerica.size)]}"
+				elsif todo_scare == 2
+					puts "#{@name} tried to scare #{@samerica[rand(@samerica.size)]}"
+				
+				end
+
+			elsif todo == 4
 				puts "scare monsters"
 			end	
 		end
